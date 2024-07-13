f0a05bd2485b13dcefb20fa947faff7b4c6b91fe
Arslan Goursi <arsljiger431@gmail.com>
2024-07-13 14:35:09 +0500

    Update main.jsx

--- client/src/screens/main.jsx
+++ client/src/screens/main.jsx
@@ -1,9 +1,32 @@
-import App from "./App.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Auth from "./auth/Index.jsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+import Dashboard from "./dashboard/Index.jsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+  {
+    path: "/auth",
+    element: <Auth />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <RouterProvider router={router} />
+    </ClerkProvider>
   </React.StrictMode>
 );
